9a81520c4f0069f58d64b22a7d5ac4539f04cbc2
lucsa <seyadeodin@gmail.com>
2026-01-14 16:43:35 -0300

    Finish component testing moudle

--- src/examples/counter/counter.test.tsx
+++ src/examples/counter/counter.test.tsx
@@ -1,10 +1,22 @@
+// @vitest-environment happy-dom
 import { screen, render } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import Counter from '.';
+import { setup } from './test/utilities';
 
-test.todo('it should render the component', () => {});
+test('it should render the component', () => {
+  render(<Counter/>)
+  //screen.debug(document.body) // serializes DOM
+});
 
-test.todo(
+test(
   'it should increment when the "Increment" button is pressed',
-  async () => {},
+  async () => {
+    const { user } = setup(<Counter/>);
+    const currentCount = await screen.findByTestId("current-count");
+    expect(currentCount).toHaveTextContent("0");
+
+    await user.click(screen.getByRole("button", {name: /increment/i }))
+    expect(currentCount).toHaveTextContent("1");
+  },
 );
+
--- src/examples/packing-list/packing-list.test.tsx
+++ src/examples/packing-list/packing-list.test.tsx
@@ -1,8 +1,18 @@
-import { render, screen } from 'test/utilities';
-import PackingList from '.';
+import { Provider } from 'react-redux';
+import { render as _render, screen } from 'test/utilities';
+import { PackingList } from '.';
+import { createStore } from './store';
+
+const render = (ui: React.ReactElement) => {
+  return _render(<Provider store={createStore()}>
+    {ui}
+  </Provider>
+  );
+
+}
 
 it('renders the Packing List application', () => {
-  render(<PackingList />);
+  render(<PackingList />)
 });
 
 it('has the correct title', async () => {
@@ -10,19 +20,56 @@ it('has the correct title', async () => {
   screen.getByText('Packing List');
 });
 
-it.todo('has an input field for a new item', () => {});
+it('has an input field for a new item', () => {
+  const { debug } = render(<PackingList />);
+  debug(document.body);
+  screen.getAllByPlaceholderText(/new item/i);
+  //screen.getByLabelText(/new item/i);
+});
 
-it.todo(
+it(
   'has a "Add New Item" button that is disabled when the input is empty',
-  () => {},
+  () => {
+    render(<PackingList />);
+
+    const inputField = screen.getByPlaceholderText(/new item/i) as any as HTMLInputElement;
+    expect(inputField.textContent).toBe("");
+
+    const addNewItemBtn = screen.getByRole("button", { name: /add new item/i }) as any as HTMLButtonElement;
+    //expect(addNewItemBtn.disabled).toBe(true);
+    expect(addNewItemBtn).toBeDisabled();
+  },
 );
 
-it.todo(
+it(
   'enables the "Add New Item" button when there is text in the input field',
-  async () => {},
+  async () => {
+    const { user } = render(<PackingList />);
+
+    const inputField = screen.getByPlaceholderText(/new item/i) as any as HTMLInputElement;
+    await user.type(inputField, "Sunglasses");
+
+    const addNewItemBtn = screen.getByRole("button", { name: /add new item/i }) as any as HTMLInputElement;
+    //expect(addNewItemBtn).toBe(false);
+    expect(addNewItemBtn).toBeEnabled();
+  },
 );
 
-it.todo(
+it(
   'adds a new item to the unpacked item list when the clicking "Add New Item"',
-  async () => {},
+  async () => {
+    const { user, debug } = render(<PackingList />);
+
+    const inputField = screen.getByPlaceholderText(/new item/i) as any as HTMLInputElement;
+    await user.type(inputField, "Sunglasses");
+
+    const addNewItemBtn = screen.getByRole("button", { name: /add new item/i }) as any as HTMLInputElement;
+    expect(addNewItemBtn).toBeEnabled();
+    await user.click(addNewItemBtn)
+
+    const unpackedItemsList = screen.getByTestId("unpacked-items");
+    //unpackedItemsList.textContent(/sunglasses/i)
+    //expect(unpackedItemsList).toContainHTML("Sunglasses")
+    expect(screen.getByLabelText('Sunglasses')).not.toBeChecked();
+  },
 );
